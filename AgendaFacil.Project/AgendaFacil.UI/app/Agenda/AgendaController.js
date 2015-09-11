127dfb49fc661e9ebe20b43c50276bc8b650bda9
Thiago901a <thiago19a@gmail.com>
2015-09-11 19:58:59 -0300

    nova estrutura

--- AgendaFacil.Project/AgendaFacil.UI/app/Agenda/AgendaController.js
+++
@@ -1,3 +0,0 @@
-﻿app.controller('AgendaCtrl', function ($rootScope, $location) {
-	$rootScope.activetab = $location.path();
-});
--- AgendaFacil.Project/AgendaFacil.UI/app/Home/HomeController.js
+++
@@ -1,3 +0,0 @@
-﻿app.controller('HomeCtrl', function ($rootScope, $location) {
-	$rootScope.activetab = $location.path();
-});
