45790c8a880c6db8d37a80e62e71c82a7d624dc4
Thiago901a <thiago19a@gmail.com>
2015-09-14 19:22:59 -0300

    designe pagina Consultar Agenda

--- AgendaFacil.Project/AgendaFacil.WebApi/app/Agenda/AgendaController.js
+++ AgendaFacil.Project/AgendaFacil.WebApi/app/Agenda/AgendaController.js
@@ -1,3 +1,16 @@
-﻿app.controller('AgendaCtrl', function ($rootScope, $location) {
-	$rootScope.activetab = $location.path();
-});
+﻿app.controller('AgendaCtrl', ['$scope', '$rootScope', function ($scope, $rootScope, $location) {
+
+    $scope.angular = 'AGENDA TESTE ANGULAR 2';
+    $scope.lstAgenda = [{ nome: "Thiago Cavalcante", data: "22-09-2015", hora: "as 14:00" }, { nome: "Quartarolo", data: "22-09-2015", hora: "as 16:00" }, { nome: "Flávia", data: "23-09-2015", hora: "as 10:00" }];
+
+    var dataAtual = '';
+    $scope.separarDatas = function (item) {
+
+        if (item != dataAtual){
+            dataAtual = item;
+            return true;
+        }
+        return false;
+    }
+
+}]);
