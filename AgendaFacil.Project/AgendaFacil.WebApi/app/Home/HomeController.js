7cd09b4c3b3dfa52124f917e4cb626f20c39059b
thiago901a <thiago19a@gmail.com>
2015-09-21 00:23:03 -0300

    melhorias designe dashboard.html e agenda.html

--- AgendaFacil.Project/AgendaFacil.WebApi/app/Home/HomeController.js
+++ AgendaFacil.Project/AgendaFacil.WebApi/app/Home/HomeController.js
@@ -1,7 +1,8 @@
 ﻿app.controller('HomeCtrl', function ($rootScope, $location) {
 
     $rootScope.activetab = $location.path();
-
+    $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+    $scope.data = [300, 500, 100];
 
 
 });
